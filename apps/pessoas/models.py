1bfd418aa59fedfca4e2b7e90b01f701f3efe554
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2024-01-30 07:54:40 -0300

    App Pessoas Criada

--- apps/pessoas/models.py
+++ apps/pessoas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Pessoa(models.Model):
+
+    OPCOES_GENERO = [
+        ('F','Feminino'),
+        ('M','Masculino'),
+    ]
+
+    nome = models.CharField(max_length=360)
+    cpf = models.CharField(max_length=11, unique=True)
+    genero = models.CharField(max_length=1, choices=OPCOES_GENERO)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
