58805c206e599db6b288b2e944cecce67c3f99b6
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2024-01-30 18:24:46 -0300

    Alterando vínculo Pessoa > Usuário
    Alterando de Foreign Key para OneToOne

--- apps/pessoas/models.py
+++ apps/pessoas/models.py
@@ -10,11 +10,13 @@ class Pessoa(models.Model):
     ]
 
     nome = models.CharField(max_length=360)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    #usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     cpf = models.CharField(max_length=11, unique=True)
     genero = models.CharField(max_length=1, choices=OPCOES_GENERO)
     email = models.EmailField()
     data_nascimento = models.DateField()
+
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
