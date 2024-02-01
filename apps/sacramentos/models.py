17ef1fec167808699873be996c95a1f982013baa
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2024-02-01 19:53:26 -0300

    Model Batismo

--- apps/sacramentos/models.py
+++ apps/sacramentos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Batismo(models.Model):
+    data = models.DateTimeField()
+    batizado = models.ForeignKey('pessoas.Paroquiano', related_name='batismos_batizado', on_delete=models.CASCADE)
+    padrinho1 = models.ForeignKey('pessoas.Paroquiano', related_name='batismos_padrinho1', on_delete=models.CASCADE)
+    padrinho2 = models.ForeignKey('pessoas.Paroquiano', related_name='batismos_padrinho2', on_delete=models.CASCADE)
+    celebrante = models.ForeignKey('pessoas.Clero', related_name='batismos_celebrante', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['batizado']
+
+    def __str__(self):
+         return f"{self.batizado} - {self.data}"
+    
