891ef19647fbf417d5470f2ae5df76f08c4ccd03
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2024-02-01 22:16:28 -0300

    Eucaristia, Crisma e Matrimonio models

--- apps/sacramentos/admin.py
+++ apps/sacramentos/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Batismo
+from .models import Batismo, Eucaristia, Crisma, Matrimonio
 
 admin.site.register(Batismo)
+admin.site.register(Eucaristia)
+admin.site.register(Crisma)
+admin.site.register(Matrimonio)
--- apps/sacramentos/models.py
+++ apps/sacramentos/models.py
@@ -13,4 +13,43 @@ class Batismo(models.Model):
 
     def __str__(self):
          return f"{self.batizado} - {self.data}"
+
+
+class Eucaristia(models.Model):
+    data = models.DateTimeField()
+    comungante = models.ForeignKey('pessoas.Paroquiano', related_name='eucaristias_comungante', on_delete=models.CASCADE)
+    celebrante = models.ForeignKey('pessoas.Clero', related_name='eucaristias_celebrante', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['comungante']
+
+    def __str__(self):
+         return f"{self.comungante} - {self.data}"
     
+
+class Crisma(models.Model):
+    data = models.DateTimeField()
+    crismado = models.ForeignKey('pessoas.Paroquiano', related_name='crismas_crismado', on_delete=models.CASCADE)
+    padrinho = models.ForeignKey('pessoas.Paroquiano', related_name='crismas_padrinho', on_delete=models.CASCADE)
+    celebrante = models.ForeignKey('pessoas.Clero', related_name='crismas_celebrante', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['crismado']
+
+    def __str__(self):
+         return f"{self.crismado} - {self.data}"
+    
+
+class Matrimonio(models.Model):
+    data = models.DateTimeField()
+    noivo = models.ForeignKey('pessoas.Paroquiano', related_name='matrimonios_noivo', on_delete=models.CASCADE)
+    noiva = models.ForeignKey('pessoas.Paroquiano', related_name='matrimonios_noiva', on_delete=models.CASCADE)
+    testemunha1 = models.ForeignKey('pessoas.Paroquiano', related_name='matrimonios_testemunha1', on_delete=models.CASCADE)
+    testemunha2 = models.ForeignKey('pessoas.Paroquiano', related_name='matrimonios_testemunha2', on_delete=models.CASCADE)
+    celebrante = models.ForeignKey('pessoas.Clero', related_name='matrimonios_celebrante', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['noivo']
+
+    def __str__(self):
+         return f"{self.noivo} e {self.noiva} - {self.data}"
