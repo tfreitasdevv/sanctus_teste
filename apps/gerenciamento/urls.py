6cd447814e8d23d2945c6ad1ff95995d211ee9ef
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2024-02-07 01:09:47 -0300

    Criação de arquivo de template base (sem conteudo)

--- apps/gerenciamento/urls.py
+++ apps/gerenciamento/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, BaseView
 
 urlpatterns = [
     path('inicio/', IndexView.as_view(), name='inicio'),
+    path('base/', BaseView.as_view(), name='base'),
 ]
--- apps/gerenciamento/views.py
+++ apps/gerenciamento/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'gerenciamento/index.html'
+
+
+class BaseView(TemplateView):
+    template_name = 'gerenciamento/base.html'
