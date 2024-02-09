88d3fbcd90e6e1cb16c711acb6bbb0472365b841
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2024-02-08 23:45:57 -0300

    Ajuste para index.html estender base.html

--- apps/gerenciamento/urls.py
+++ apps/gerenciamento/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import IndexView, BaseView
 
 urlpatterns = [
-    path('inicio/', IndexView.as_view(), name='inicio'),
     path('base/', BaseView.as_view(), name='base'),
+    path('', IndexView.as_view(), name='inicio'),
 ]
